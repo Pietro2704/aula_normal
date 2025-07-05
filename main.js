4489bc86a09c1620b58d0f467817b86852bdebd1
Pietro <pietro.rosolia.2704@gmail.com>
2025-07-05 14:49:42 -0300

    criei uma funcao chamada tabuada que retorna a tabuada de um numero

---
+++ main.js
@@ -0,0 +1,11 @@
+function Tabuada(numero, inicio=1, fim=10){
+
+    let texto = ""
+
+    for(let i = inicio; i <= fim; i++){
+        texto += `${numero} X ${i} = ${numero*i}\n`
+    }
+    alert(texto)
+}
+
+Tabuada(3, 2, 6)
